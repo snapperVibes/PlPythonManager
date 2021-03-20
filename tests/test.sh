40e326ced089b9f88e1554ed58e5fd1d4537c0da
Snapper <learningwithsnapper@gmail.com>
2021-03-20 16:12:33 -0400

    Moved docker-compose out of root directory

--- tests/test.sh
+++ tests/test.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
-trap "docker-compose down -v --remove-orphans" EXIT
+trap "docker-compose -f tests/docker-compose.yml down -v --remove-orphans" EXIT
 
 # Exit in case of error
 set -e
 
-docker-compose down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
+docker-compose -f tests/docker-compose.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 
 if [ $(uname -s) = "Linux" ]; then
     echo "Remove __pycache__ files"
     sudo find . -type d -name __pycache__ -exec rm -r {} \+
 fi
 
-docker-compose build
-docker-compose up -d
+docker-compose -f tests/docker-compose.yml build
+docker-compose -f tests/docker-compose.yml up -d
 #sleep 3
-docker-compose exec -T tester pytest /src/tests
+docker-compose -f tests/docker-compose.yml exec -T tester pytest /src/tests/
